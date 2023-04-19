7b2a57c3b3616ec282802b8a20325102acdd6b58
K-Waks <wakskevin@outlook.com>
2023-04-19 16:09:09 +0300

    update

---
+++ roughwork/program.c
@@ -0,0 +1,23 @@
+int sum(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n + sum(n - 1));
+    }
+}
+
+
+int main()
+{
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    printf("Sum of numbers from 0 till %d is %d.", n, sum(n));
+    return (0);
+}
+
+// if (232 a3 >= "ast" || 23.5 <= 90.s3) do {a2 = 89}
